9515a308d5e112cfccf4f8d59a41f834f6e4c7e3
almalikzakwan <almalikzakwan@gmail.com>
2026-05-04 07:35:10 +0800

    split punctuation into function

--- random_characters.py
+++ random_characters.py
@@ -1,16 +1,26 @@
 import random
 import string 
+from enum import Enum
 
-def random_characters(k=16):
-    all_punctuation = string.punctuation
-    punctuations = random.choices(all_punctuation, k=16)
-    punctuation = random.choice(all_punctuation)
-    pub_key = f""
+class t(Enum):
+    punctuation = 1
+    punctuations = 2
+
+def punctuation(type):
+    match type:
+        case t.punctuation:
+            return random.choice(string.punctuation)
+        case t.punctuations:
+            return string.punctuation
+        
+def random_characters(k=128):
+    pub_key = f"public_key"
     ascii_letter = string.ascii_letters
     hexdigits = string.hexdigits
     # random_characters = ascii_letter + ''.join(punctuations) + hexdigits
     random_characters = ascii_letter + hexdigits
     # return pub_key + punctuation + ''.join(random.choices(list(random_characters), k=k))
-    return pub_key + ''.join(random.choices(list(random_characters), k=k))
+    return pub_key + punctuation(t.punctuation) + ''.join(random.choices(list(random_characters), k=k))
 
 print(random_characters())
+
